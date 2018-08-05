c34a4014e0d5792df1b570b670c020906d6d2465
osdnk <micosa97@gmail.com>
2018-08-06 00:24:36 +0200

    Fix module and example :tada:

--- android/src/main/java/com/osdnk/haptics/RNHapticsModule.java
+++ android/src/main/java/com/osdnk/haptics/RNHapticsModule.java
@@ -3,17 +3,18 @@ package com.osdnk.haptics;
 
 import android.content.Context;
 import android.os.Vibrator;
-import android.view.View;
-import 	android.view.HapticFeedbackConstants;
 
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
+
 public class RNHapticsModule extends ReactContextBaseJavaModule {
- // private final Vibrator mVibrator =  (Vibrator) getCurrentActivity().getApplicationContext().getSystemService(Context.VIBRATOR_SERVICE);
+  private final RNHapticsPerformer mPerformer;
 
   public RNHapticsModule(ReactApplicationContext reactContext) {
     super(reactContext);
+    Vibrator vibrator = (Vibrator) reactContext.getSystemService(Context.VIBRATOR_SERVICE);
+    mPerformer = new RNHapticsPerformer(vibrator);
   }
 
   @Override
@@ -23,15 +24,36 @@ public class RNHapticsModule extends ReactContextBaseJavaModule {
 
   @ReactMethod
   public void notification(String type){
+    switch (type) {
+      case "error":
+        mPerformer.notificationError();
+        break;
+      case "warning":
+        mPerformer.notificationWarning();
+        break;
+      default:
+        mPerformer.notificationSuccess();
+        break;
+    }
   }
 
   @ReactMethod
   public void selection(){
-
+    mPerformer.selection();
   }
 
   @ReactMethod
   public void impact(String type){
-
+    switch (type) {
+      case "light":
+        mPerformer.impactLight();
+        break;
+      case "heavy":
+        mPerformer.impactHeavy();
+        break;
+      default:
+        mPerformer.impactMedium();
+        break;
+    }
   }
 }
--- example/App.js
+++ example/App.js
@@ -1,21 +1,11 @@
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View, Button} from 'react-native';
+import { StyleSheet, Text, View, Button} from 'react-native';
 import RNH from 'react-native-haptics'
-
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
-
 export default class App extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+        <Text> Won't u haptic? </Text>
         <Button title="N S" onPress={() => RNH.notification("success")}/>
         <Button title="N W" onPress={() => RNH.notification("warning")}/>
         <Button title="N E" onPress={() => RNH.notification("error")}/>
